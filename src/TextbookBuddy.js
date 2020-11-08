7ef678e8fa020360fc76fd79ab133037ec2d8ec8
Nykolas Farhangi <nykolasfarhangi@gmail.com>
2020-11-08 03:51:10 -0600

    merged

--- src/TextbookBuddy.js
+++ src/TextbookBuddy.js
@@ -35,13 +35,8 @@ const SearchComponent = ( { getTextbookSearchResults } ) => {
 
 const TextbookBuddy = ( { props, getTextbookSearchResults } ) => {
     return (
-<<<<<<< HEAD
         <div className="section">
             <div className="title"> BOOKS </div>
-=======
-        <div class="section">
-            <div class="title"> <b>BOOKS</b> </div>
->>>>>>> homepage
 
             <SearchComponent getTextbookSearchResults={ getTextbookSearchResults } />
 
